0d5baa63be804191d43311a85993bb93cfaf9274
Aleksey Pivkin <a.1.3@mail.ru>
2022-10-07 11:38:17 +0300

    chore: added colored message in merge command

--- src/commands/merge-branch.ts
+++ src/commands/merge-branch.ts
@@ -1,5 +1,6 @@
 import { exec } from './exec';
 import { clc } from '../color';
+import inquirer from 'inquirer';
 
 //
 export async function mergeBranch(branchName: string) {
@@ -7,10 +8,39 @@ export async function mergeBranch(branchName: string) {
     throw new Error('No branch...');
   }
 
-  const checkoutCommand = `git merge ${branchName}`;
+  const currentBranch = await exec('git rev-parse --abbrev-ref HEAD');
 
-  console.log(' ' + clc.green('git merge', branchName) + '\n');
+  const message = [
+    clc._blue,
+    'Do you want to merge branch ',
+    clc._green,
+    branchName, // branch
+    clc._blue,
+    ' into ',
+    clc._green,
+    currentBranch, // branch
+    clc._blue,
+    '?',
+    clc._off,
+  ].join('');
 
-  const checkoutCommandResult = await exec(checkoutCommand);
-  console.log(checkoutCommandResult);
+  const res = await inquirer.prompt({
+    type: 'confirm',
+    name: 'answer',
+    message,
+    default: false,
+  });
+
+  if (res.answer === true) {
+    const checkoutCommand = `git merge ${branchName}`;
+
+    console.log(' ' + clc.green('git merge', branchName) + '\n');
+
+    const checkoutCommandResult = await exec(checkoutCommand);
+    console.log(checkoutCommandResult);
+
+    return;
+  }
+
+  console.log(clc.yellow('The branch was not merged'));
 }
